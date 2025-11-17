9698ac1adfbf63affdc68caed15c30b322248d34
Lucas Santos <lucassantos135b211@gmail.com>
2025-11-17 20:44:29 -0300

    Update Program.cs

--- Projeto_filas_acessos/Projeto_filas_acessos/Program.cs
+++ Projeto_filas_acessos/Projeto_filas_acessos/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_filas_acessos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Salve");
+
+        }
+    }
+}
